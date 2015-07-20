9a401ab1a71a3b90385be5756b643873d77d8158
liqites <leechee89@gmail.com>
2015-07-20 22:22:55 +0800

    fix

--- weixin_app/app/controllers/messages_controller.rb
+++ weixin_app/app/controllers/messages_controller.rb
@@ -5,26 +5,36 @@ class MessagesController < ApplicationController
 	def receive
 		#收到信息时，记录下用户和签到时间
 		msgdata =  params[:xml]
+		msg = ""
+		ActiveRecord::Base.transaction do 
+		begin
+			
+			#创建发送用户
+			from_wx_user = WeixinUser.find_by_openid(msgdata[:FromUserName])
+			if from_wx_user.nil?
+				from_wx_user = WeixinUser.create({openid:msgdata[:FromUserName]})
+			end
 
-		#创建发送用户
-		from_wx_user = WeixinUser.find_by_openid(msgdata[:FromUserName])
-		if from_wx_user.nil?
-			from_wx_user = WeixinUser.create({openid:msgdata[:FromUserName]})
+			#创建公众号用户
+			to_wx_user = WeixinUser.find_by_name(msgdata[:ToUserName])
+			if to_wx_user.nil?
+				to_wx_user = WeixinUser.create({name:msgdata[:ToUserName]})
+			end
+
+			#创建消息
+			message = Message.create({to_user:msgdata[:ToUserName],from_user:msgdata[:FromUserName],create_time:Time.new(msgdata[:CreateTime]),msg_type:msgdata[:MsgType],content:msgdata[:Content],msg_id:msgdata[:MsgId]})
+
+			#签到逻辑
+			#-------------
+			msg = Attendence.create_attendence(message,from_wx_user)
+			#-------------
+			
+		rescue => e
+			puts e.message
+			puts e.backtrace
+			msg = ""
 		end
-
-		#创建公众号用户
-		to_wx_user = WeixinUser.find_by_name(msgdata[:ToUserName])
-		if to_wx_user.nil?
-			to_wx_user = WeixinUser.create({name:msgdata[:ToUserName]})
 		end
-
-		#创建消息
-		message = Message.create({to_user:msgdata[:ToUserName],from_user:msgdata[:FromUserName],create_time:Time.new(msgdata[:CreateTime]),msg_type:msgdata[:MsgType],content:msgdata[:Content],msg_id:msgdata[:MsgId]})
-
-		#签到逻辑
-		#-------------
-		msg = Attendence.create_attendence(message,from_wx_user)
-		#-------------
 
 		render json: {result:msg}
 	end
--- weixin_app/app/models/attendence.rb
+++ weixin_app/app/models/attendence.rb
@@ -1,8 +1,8 @@
 class Attendence < ActiveRecord::Base
   belongs_to :weixin_user
 
-  START_TIME = 6
-	END_TIME = 8
+  START_TIME = 22
+	END_TIME = 23
 
   def self.create_attendence(message,user)
     content = message.content
@@ -10,13 +10,34 @@ class Attendence < ActiveRecord::Base
     # 如果在6~8之间，那么，有“签到”两个字的，则符合返回条件
     # 在其他时间段，只有完全匹配“签到”两个字，才会返回签到失败
 
-  	hour = Time.now.hour
-  	min = Time.now.min
+  	if condition
+      unless /签到/ =~ content
+        return ""
+      end
+    else
+      if content == "签到"
+        return "签到失败"
+      else
+        return ""
+      end
+    end
+
   	create({weixin_user_id:user.id,content: message.content,message_id:message.id})
-  	if (hour >= START_TIME && hour < END_TIME) || (hour == END_TIME && min == 0)
+  	if condition && (/签到/ =~ content)
   		return "签到成功"
   	else
   		return "签到失败"
   	end
+  end
+
+  private
+  def self.condition
+    hour = Time.now.hour
+    min = Time.now.min
+    if (hour >= START_TIME && hour < END_TIME) || (hour == END_TIME && min == 0)
+      true
+    else
+      false
+    end
   end
 end
